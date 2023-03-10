6a6526733fc05cc09fcd8166d6868f499f5e6170
Sravan T P <sravantp03@gmail.com>
2023-03-10 12:32:25 +0530

    changed network config to sepolia testnet

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,8 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const ACCOUNT_PRIVATE_KEY = process.env.ACCOUNT_PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -10,8 +10,8 @@ module.exports = {
     defaultNetwork: "goerli",
     networks: {
         goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: [GOERLI_PRIVATE_KEY],
+            url: SEPOLIA_RPC_URL,
+            accounts: [ACCOUNT_PRIVATE_KEY],
         },
     },
     etherscan: {
